dd7c2b80823c4921133336daf470917b8c66480d
Jonas Jasulevicius <jonas.jasul@gmail.com>
2023-09-14 15:10:21 +0300

    Added some reveal animations

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,71 +1,79 @@
 import AnimatedCaret from "./animatedCaret";
 import { DiGithubFull } from 'react-icons/di';
 import { BsLinkedin } from 'react-icons/bs';
+import { Slide, Bounce } from "react-awesome-reveal";
 export default function About() {
     return (
+
 
         <div className="main">
             <div className="flex flex-col lg:flex-row lg:h-[100vh]">
                 <div className="left-cont w-2/3 mt-16 lg:mt-44 ml-16">
-                    <div className="left-text w-2/3">
-                        <h2 className="text-6xl">
-                            <span className="hello">Labas, aš</span> <span className="text-emerald-500">Jonas Jasulevičius</span><span className="hello">!</span>
-                        </h2>
-                    </div>
-                    <div className="code-block bg-slate-700 rounded-lg mt-2 w-11/12 lg:w-4/6 md:w-2/3">
-                        <div className="code-top bg-slate-600 rounded-lg flex flex-row">
-                            <div className="circles-cont flex flex-row justify-center items-center p-2">
-                                <div className="circle w-3 h-3 rounded-full bg-red-400 mx-1"></div>
-                                <div className="circle w-3 h-3 rounded-full bg-orange-300 mx-1"></div>
-                                <div className="circle w-3 h-3 rounded-full bg-lime-300 mx-1"></div>
+                    <Slide triggerOnce={true}>
+                        <div className="left-text w-2/3">
+                            <h2 className="text-6xl">
+                                <span className="hello">Labas, aš</span> <span className="text-emerald-500">Jonas Jasulevičius</span><span className="hello">!</span>
+                            </h2>
+                        </div>
+                        <div className="code-block bg-slate-700 rounded-lg mt-2 w-11/12 lg:w-4/6 md:w-2/3">
+                            <div className="code-top bg-slate-600 rounded-lg flex flex-row">
+                                <div className="circles-cont flex flex-row justify-center items-center p-2">
+                                    <div className="circle w-3 h-3 rounded-full bg-red-400 mx-1"></div>
+                                    <div className="circle w-3 h-3 rounded-full bg-orange-300 mx-1"></div>
+                                    <div className="circle w-3 h-3 rounded-full bg-lime-300 mx-1"></div>
+                                </div>
+
+                                <div className="code-tab h-7 flex flex-row justify-center items-center p-3 rounded-tl-lg rounded-tr-lg bg-slate-700 ml-1" style={{ fontFamily: "HackRegular" }}>
+                                    <img className="w-4 h-4 mr-1" src="/assets/html_icon.png" /><span className="text-white text-xs">Jonas.html</span>
+                                </div>
                             </div>
-
-                            <div className="code-tab h-7 flex flex-row justify-center items-center p-3 rounded-tl-lg rounded-tr-lg bg-slate-700 ml-1" style={{ fontFamily: "HackRegular" }}>
-                                <img className="w-4 h-4 mr-1" src="/assets/html_icon.png" /><span className="text-white text-xs">Jonas.html</span>
+                            <div className="code-text p-3" style={{ fontFamily: "HackRegular" }}>
+                                <p className="text-xl mt-1 text-white">
+                                    &lt;<span className="text-red-400">h3</span>&gt;Esu tinklalapių programuotojas&lt;/<span className="text-red-400">h3</span>&gt;
+                                    <br /> <br />
+                                    &lt;<span className="text-red-400">p</span>&gt;Mano aistra - programavimas ir problemų sprendimas, naudojant naujausias technologijas.&lt;/<span className="text-red-400">p</span>&gt;
+                                    <span className="animated-caret text-blue-400"><AnimatedCaret /></span>
+                                </p>
                             </div>
                         </div>
-                        <div className="code-text p-3" style={{ fontFamily: "HackRegular" }}>
-                            <p className="text-xl mt-1 text-white">
-                                &lt;<span className="text-red-400">h3</span>&gt;Esu tinklalapių programuotojas&lt;/<span className="text-red-400">h3</span>&gt;
-                                <br /> <br />
-                                &lt;<span className="text-red-400">p</span>&gt;Mano aistra - programavimas ir problemų sprendimas, naudojant naujausias technologijas.&lt;/<span className="text-red-400">p</span>&gt;
-                                <span className="animated-caret text-blue-400"><AnimatedCaret /></span>
-                            </p>
-                        </div>
-                    </div>
 
-                    <div className="links text mt-10 flex">
-                        <div className="github m-3">
-                            <a href="https://github.com/jonas-jasul">
-                                <button>
-                                    <DiGithubFull size={'6em'} />
-                                </button>
-                            </a>
+                        <div className="links text mt-10 flex">
+                            <div className="github m-3">
+                                <a href="https://github.com/jonas-jasul">
+                                    <button>
+                                        <DiGithubFull size={'6em'} />
+                                    </button>
+                                </a>
 
+                            </div>
+                            <div className="linkedin m-3" >
+                                <a href="https://www.linkedin.com/in/jonas-jasulevicius-648177211/">
+                                    <button>
+                                        <BsLinkedin size={'4em'} />
+                                    </button>
+                                </a>
+                            </div>
                         </div>
-                        <div className="linkedin m-3" >
-                            <a href="https://www.linkedin.com/in/jonas-jasulevicius-648177211/">
-                                <button>
-                                    <BsLinkedin size={'4em'} />
-                                </button>
-                            </a>
-                        </div>
-                    </div>
+                    </Slide>
                 </div>
+
 
                 <div className="right-cont w-full mt-3 lg:mt-0 lg:w-1/3 pt-6 h-full lg:pt-44">
                     <div className="pic-cont h-20 md:h-48 lg:h-80">
-                        <img className="object-contain absolute right-[26%] md:right-[36%] lg:right-[20%] rounded-full w-32 lg:w-80 md:w-64" src="/assets/me.jpg"></img>
+                        <img className="object-contain absolute right-[26%] md:right-[36%] lg:right-[20%] w-32 lg:w-80 md:w-64" src="/assets/me.jpg"></img>
                     </div>
-                    <div className="cvbuttons mx-2">
+
+                    <div className="cvbuttons mx-2 my-1">
                         <a href="\assets\cv.pdf" download="Jonas CV" target="_blank">
                             <button className="button p-2 border-2 rounded-xl hover:bg-gray-200 hover:text-black hover:transition-all border-emerald-500">Atsisiųsti CV</button>
                         </a>
                     </div>
                 </div>
             </div>
+
             <div className="transition-line lg:w-full lg:h-4 "></div>
         </div>
+
     )
 }
 
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -3,9 +3,11 @@ import { BsGlobe, BsWindow, BsPhone, BsDatabase } from 'react-icons/bs';
 import { TbApi } from 'react-icons/tb'
 import { BiLogoReact, BiLogoJavascript, BiLogoHtml5, BiLogoCss3, BiLogoTailwindCss } from 'react-icons/bi';
 import { SiNextdotjs, SiMicrosoftazure, SiPhp, SiCsharp } from 'react-icons/si';
+import { Slide, Fade } from 'react-awesome-reveal';
 export default function Skills() {
 
     return (
+
         <div className="flex flex-col lg:flex-row lg:h-[70vh]">
             <div className="skills-left-cont w-full h-2/5 lg:w-2/5 lg:h-full flex flex-col justify-center items-center p-2">
                 <h2 className=" text-4xl my-6 text-emerald-400">Mano įgūdžiai</h2>
@@ -13,6 +15,7 @@ export default function Skills() {
                     <p className="text-2xl text-center p-3">Aš niekada nenustoju mokytis,
                         domiuosi naujomis programavimo technologijomis ir vis tobulėju.</p>
 
+                    <Fade cascade triggerOnce={true} fraction={0.7}>
                     <ul className='flex justify-center items-center flex-col pb-3'>
                         <li className='flex items-center text-xl'>
                             <BsGlobe className="text-cyan-300 mr-1" />
@@ -32,54 +35,58 @@ export default function Skills() {
                             Integruoju REST API
                         </li>
                     </ul>
+                    </Fade>
                 </div>
             </div>
-            <div className="skills-right-cont w-full lg:w-3/5 flex justify-center items-center">
-                <div className='skill-logos flex flex-wrap m-20 justify-center'>
-                    <div className='p-2 hover:text-blue-400'>
-                        <BiLogoReact className="mx-auto" size={'4em'} />
-                        <p className='text-center'>React.js</p>
-                    </div>
-                    <div className='p-2 hover:text-black'>
-                        <SiNextdotjs className="mx-auto" size={'4em'} />
-                        <p className='text-center'>Next.js</p>
-                    </div>
-                    <div className='p-2 hover:text-yellow-300'>
-                        <BiLogoJavascript className="mx-auto" size={'4em'} />
-                        <p className='text-center'>JavaScript</p>
-                    </div>
-                    <div className='p-2 hover:text-cyan-300'>
-                        <BsDatabase className="mx-auto" size={'4em'} />
-                        <p className='text-center'>SQL</p>
-                    </div>
-                    <div className='p-2 hover:text-orange-500'>
-                        <BiLogoHtml5 className="mx-auto" size={'4em'} />
-                        <p className='text-center'>HTML</p>
-                    </div>
+            <Slide direction='right' triggerOnce={true} fraction={0.6}>
+                <div className="skills-right-cont w-full lg:w-3/5 flex justify-center h-full items-center mx-auto my-auto">
+                    <div className='skill-logos flex flex-wrap m-20 justify-center'>
+                        <div className='p-2 hover:text-blue-400'>
+                            <BiLogoReact className="mx-auto" size={'4em'} />
+                            <p className='text-center'>React.js</p>
+                        </div>
+                        <div className='p-2 hover:text-black'>
+                            <SiNextdotjs className="mx-auto" size={'4em'} />
+                            <p className='text-center'>Next.js</p>
+                        </div>
+                        <div className='p-2 hover:text-yellow-300'>
+                            <BiLogoJavascript className="mx-auto" size={'4em'} />
+                            <p className='text-center'>JavaScript</p>
+                        </div>
+                        <div className='p-2 hover:text-cyan-300'>
+                            <BsDatabase className="mx-auto" size={'4em'} />
+                            <p className='text-center'>SQL</p>
+                        </div>
+                        <div className='p-2 hover:text-orange-500'>
+                            <BiLogoHtml5 className="mx-auto" size={'4em'} />
+                            <p className='text-center'>HTML</p>
+                        </div>
 
-                    <div className='p-2 hover:text-cyan-600'>
-                        <BiLogoCss3 className='mx-auto' size={'4em'} />
-                        <p className='text-center'>CSS</p>
-                    </div>
-                    <div className='p-2 hover:text-teal-400'>
-                        <BiLogoTailwindCss className='mx-auto' size={'4em'} />
-                        <p className='text-center'>Tailwind CSS</p>
-                    </div>
-                    <div className='p-2 hover:text-sky-600'>
-                        <SiMicrosoftazure className='mx-auto' size={'4em'} />
-                        <p className='text-center'>Azure</p>
-                    </div>
-                    <div className='p-2 hover:text-indigo-400'>
-                        <SiPhp className='mx-auto' size={'4em'} />
-                        <p className='text-center'>PHP</p>
-                    </div>
-                    <div className='p-2 hover:text-violet-600'>
-                        <SiCsharp className='mx-auto' size={'4em'} />
-                        <p className='text-center'>C#</p>
-                    </div>
+                        <div className='p-2 hover:text-cyan-600'>
+                            <BiLogoCss3 className='mx-auto' size={'4em'} />
+                            <p className='text-center'>CSS</p>
+                        </div>
+                        <div className='p-2 hover:text-teal-400'>
+                            <BiLogoTailwindCss className='mx-auto' size={'4em'} />
+                            <p className='text-center'>Tailwind CSS</p>
+                        </div>
+                        <div className='p-2 hover:text-sky-600'>
+                            <SiMicrosoftazure className='mx-auto' size={'4em'} />
+                            <p className='text-center'>Azure</p>
+                        </div>
+                        <div className='p-2 hover:text-indigo-400'>
+                            <SiPhp className='mx-auto' size={'4em'} />
+                            <p className='text-center'>PHP</p>
+                        </div>
+                        <div className='p-2 hover:text-violet-600'>
+                            <SiCsharp className='mx-auto' size={'4em'} />
+                            <p className='text-center'>C#</p>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
+            </Slide>
         </div>
+
     )
 }
